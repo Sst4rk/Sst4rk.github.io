3e6fec124c0351e2759edb1f44119300617d66fb
sst4rk <>
2021-11-08 22:29:53 -0600

    version 2.08

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
-function menuOpener(){let e=document.getElementById("navegador");e.classList.add("navegadorOpen"),e.classList.remove("navegadorClose")}function menuCloser(){let e=document.getElementById("navegador");e.classList.add("navegadorClose"),e.classList.remove("navegadorOpen")}
+function menuOpener(){
+    let e=document.getElementById("navegador");
+    e.classList.add("navegadorOpen"),e.classList.remove("navegadorClose")
+}
+function menuCloser(){
+    let e=document.getElementById("navegador");
+    e.classList.add("navegadorClose"),e.classList.remove("navegadorOpen")
+}
+
+
+
+window.onscroll = function() {
+    let boton =document.getElementById("botonArriba");
+    if (window.scrollY>500)
+        boton.style.opacity="100%";
+    else{
+        boton.style.opacity="0%";
+        
+    }
+  };
