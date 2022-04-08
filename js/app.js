02efa10916a2bb37660e2a9cb3f94d48804fcd7a
sst4rk <you@example.com>
2022-04-08 09:41:37 -0500

    Empezando version 2 con test

--- js/app.js
+++ js/app.js
@@ -1,26 +1,25 @@
-function menuOpener(){
-    let e=document.getElementById("navegador");
-    e.classList.add("navegadorOpen"),e.classList.remove("navegadorClose")
+function menuOpener() {
+    let e = document.getElementById("navegador");
+    e.classList.add("navegadorOpen"), e.classList.remove("navegadorClose")
 }
-function menuCloser(){
-    let e=document.getElementById("navegador");
-    e.classList.add("navegadorClose"),e.classList.remove("navegadorOpen")
+
+function menuCloser() {
+    let e = document.getElementById("navegador");
+    e.classList.add("navegadorClose"), e.classList.remove("navegadorOpen")
 }
 
 
 window.onscroll = function() {
-    let boton =document.getElementById("botonArriba");
-    if (window.scrollY>500)
-        boton.style.opacity="100%";
-    else{
-        boton.style.opacity="0%";
-        
+    let boton = document.getElementById("botonArriba");
+    if (window.scrollY > 500)
+        boton.style.opacity = "100%";
+    else {
+        boton.style.opacity = "0%";
+
     }
-  };
+};
 
 
-//let texto = "Bienvenidos a STAR SYSTEM"
-
-//const hablar = (texto) => speechSynthesis.speak(new SpeechSynthesisUtterance(texto))
-
-//hablar(texto);
+(() => {
+    fetch("https://google.com").then(res => { console.log(res); });
+})();
